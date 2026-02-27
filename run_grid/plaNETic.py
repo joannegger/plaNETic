7addcb49819e8a53afdf9362fd80619eadc492ec
Egger, Jo Ann (je18d174) <jo-ann.egger@space.unibe.ch>
2026-02-27 17:36:27 +0100

    Generate 'posteriors' and 'plots' folders automatically

--- run_grid/plaNETic.py
+++ run_grid/plaNETic.py
@@ -2112,6 +2112,12 @@ def make_slurm_submit_file(n_samples, comp_option_mass, comp_option_radius, with
 # Executables
 # --------------------------------------------------------------------------------------------------------------------------------------------------------
 def compute_posterior(comp_option_mass, comp_option_radius, SiMgFe_ratio_option, water_mixing_option, luminosity_option, use_log_prior_for_gas_mass=True, with_gas=True, with_water=True, csv_file='stellar_planetary_parameters.csv', comment='', verbose=True, save_prior=True, date_string='auto'):
+    if not os.path.exists('posteriors'):
+        os.makedirs('posteriors')
+    
+    if not os.path.exists('plots'):
+        os.makedirs('plots')
+    
     obs, nplanets_total, nsystems_total = read_csv_info(comp_option_mass, comp_option_radius, with_gas, with_water, csv_file, comment=comment, verbose=verbose, date_string=date_string)
 
     count_m1 = 0
